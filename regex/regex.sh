72d0d3dc607f9b66d554ef7fd393b42b9bd96568
Blake Johnson <joh18484@morris.umn.edu>
2021-09-08 20:45:38 -0500

    Updated regex.sh Added lines for commands that will extract content from r1_input.txt and r2_input.txt and display as shown in the README.

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 
-sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n 2. \2\n/' < r0_input.txt > r0_output.txt
+sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
 
 
+sed -E 's/\* I am ([a-zA-Z]+)\. My favorite sandwhich is ([a-zA-Z]+)\./1. \1\n2. \2\n/;t;d' < r1_input.txt > r1_output.txt
 
-#sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r1_input.txt > r1_output.txt
+
+sed -E 's/\* sandwhich with ([a-zA-Z\.]+) ([a-zA-Z]+)/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt
 
