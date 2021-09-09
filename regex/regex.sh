4e4c6cfe3acdf9acae0d5b4e517061a29c2eeb0b
Blake Johnson <joh18484@morris.umn.edu>
2021-09-08 20:57:04 -0500

    Updated regex.sh Corrected spelling of sandwhich to sandwich.

--- regex/regex.sh
+++ regex/regex.sh
@@ -4,8 +4,8 @@
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
 
 
-sed -E 's/\* I am ([a-zA-Z]+)\. My favorite sandwhich is ([a-zA-Z]+)\./1. \1\n2. \2\n/;t;d' < r1_input.txt > r1_output.txt
+sed -E 's/\* I am ([a-zA-Z]+)\. My favorite sandwich is ([a-zA-Z]+)\./1. \1\n2. \2\n/' < r1_input.txt > r1_output.txt
 
 
-sed -E 's/\* sandwhich with ([a-zA-Z\.]+) ([a-zA-Z]+)/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt
+sed -E 's/\* sandwich with ([a-zA-Z\.]+) ([a-zA-Z]+)/1. \1\n2. \2\n/' < r2_input.txt > r2_output.txt
 
